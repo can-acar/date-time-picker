659f45fb37b28afa96c66dc4210ce422fd8e35fd
Can Acar <can.acar2@ogr.sakarya.edu.tr>
2023-04-02 00:20:07 +0300

    1.0.0

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import Client from "react-dom/client";
 
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 import moment from "moment";
 
-import "./index.css"
+import "./index.css";
 
-ReactDOM.render(
+Client.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
